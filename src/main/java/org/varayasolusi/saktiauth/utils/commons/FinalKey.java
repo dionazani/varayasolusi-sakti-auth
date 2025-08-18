60aee108f3b813a5ef62349d7c5435af1a8a1144
Dion Azani <dion.azani@gmail.com>
2025-08-18 20:11:05 +0700

    Enhance Authenticated

---
+++ src/main/java/org/varayasolusi/saktiauth/utils/commons/FinalKey.java
@@ -0,0 +1,8 @@
+package org.varayasolusi.saktiauth.utils.commons;
+
+public class FinalKey {
+
+    public static final String USERAUTHENTICATED_AUTHENTICATIONTYPE = "INTERNAL";
+    public static final String USERAUTHENTICATED_TOKENTYPE = "BEARER";
+    public static final String USERAUTHENTICATED_BEHAVIOUR = "L";
+}
